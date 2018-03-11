5b9ff20af945c4b7e41fe11dfcdcd9322264bff5
Florian-Geyer <dmitmaks@yandex.ru>
2018-03-11 14:15:59 +0400

    First commit

---
+++ testforjava/src/test.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class test {
+
+    public static void main(String args[]) throws IOException{
+
+        int a = 100;
+
+        int b = 100-20;
+
+        int z = a/b;
+
+        System.out.println(z);
+    }
+
+}
